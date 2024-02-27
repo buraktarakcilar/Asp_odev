4c65ff76d5286bd69a5e9e545c49f9cb28afb5fa
buraktrkcilar <148910370+buraktarakcilar@users.noreply.github.com>
2024-02-27 08:14:49 +0200

    a

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using _20_02_2024Ev.Models;
+
+namespace _20_02_2024Ev.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        ViewData["Name"]="Burak Tarakçılar";
+        return View();
+    }
+    public IActionResult Numbers()
+    {
+      
+    
+        Random rnd = new Random();
+        ViewBag.Numara = rnd.Next(1, 100);
+    
+    
+        
+        return View();
+    }
+  
+    public IActionResult Lessons()
+    {
+       string [] lessonList =  { "Mobil Uygulamalar","Veri Tabanlı İçerik Yönetimi","Web Tabanlı İçerik Yönetimi","Oyun  programlama"};
+       ViewBag.lessons=lessonList;
+       
+       
+        
+
+        return View();
+    }
+    
+
+   
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _20_02_2024Ev.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
